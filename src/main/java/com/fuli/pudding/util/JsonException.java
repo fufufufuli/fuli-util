ca26ea7e5574ea8e3d1d89adcdff69c95bff7416
fuli <linfuli@magfin.cn>
2019-07-08 10:03:38 +0800

    feat:     code refine

--- src/main/java/com/fuli/pudding/util/JsonException.java
+++ src/main/java/com/fuli/pudding/util/JsonException.java
@@ -1,16 +1,12 @@
 package com.fuli.pudding.util;
 
 /**
- * Created by kui.luo on 14-2-20.
+ * @author fuli
  */
 public final class JsonException extends RuntimeException {
-    static final String GET_VALUE_ERROR = "获取json中key值异常";
+    public static final String GET_VALUE_ERROR = "获取json中key值异常";
 
-    public JsonException(final String message) {
-        super(message);
-    }
+    public JsonException(final String message) { super(message); }
 
-    public JsonException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+    public JsonException(final String message, final Throwable cause) { super(message, cause); }
 }
---
+++ src/test/java/com/fuli/pudding/util/BaseTest.java
@@ -0,0 +1,14 @@
+package com.fuli.pudding.util;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author fuli
+ */
+public class BaseTest {
+    @Test
+    public void test() throws Exception {
+        System.out.println(1);
+    }
+}
