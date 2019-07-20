34edfcfc5ae9a602a8fffe77681619e3f10c1386
fuli <linfuli@magfin.cn>
2019-07-20 14:59:19 +0800

    feat:      refine

--- src/main/java/com/fuli/util/JsonUtil.java
+++ src/main/java/com/fuli/util/JsonUtil.java
@@ -2,12 +2,10 @@ package com.fuli.util;
 
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -20,7 +18,6 @@ public class JsonUtil {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final XmlMapper XML_MAPPER = new XmlMapper();
     private static final JavaTimeModule JAVA_TIME_MODULE = new JavaTimeModule();
-    private static final ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
 
     static {
         OBJECT_MAPPER.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
@@ -29,8 +26,6 @@ public class JsonUtil {
 
         XML_MAPPER.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
         XML_MAPPER.registerModule(JAVA_TIME_MODULE);
-
-        YAML_MAPPER.findAndRegisterModules();
     }
 
     /**
@@ -155,55 +150,5 @@ public class JsonUtil {
             throw new JsonException(JsonException.GET_VALUE_ERROR, e);
         }
     }
-
-    public static <T> T fromYaml(String yaml, Class<T> clazz) {
-        try {
-            return YAML_MAPPER.readValue(yaml, clazz);
-        } catch (IOException e) {
-            throw new JsonException(e.getLocalizedMessage());
-        }
-    }
-
-    public static <T> T fromYaml(File yamlFile, Class<T> clazz) {
-        try {
-            return YAML_MAPPER.readValue(yamlFile, clazz);
-        } catch (IOException e) {
-            throw new JsonException(e.getLocalizedMessage());
-        }
-    }
-
-    /**
-     * 获取泛型的Collection Type
-     *
-     * @param yamlFile        yaml文件
-     * @param collectionClass 泛型的Collection
-     * @param elementClasses  元素类型
-     */
-    public static <T> T fromYamlFile(File yamlFile, Class<?> collectionClass, Class<?>... elementClasses) {
-        JavaType javaType = YAML_MAPPER.getTypeFactory().constructParametricType(collectionClass, elementClasses);
-        try {
-            return YAML_MAPPER.readValue(yamlFile, javaType);
-        } catch (IOException e) {
-            throw new JsonException("convert json error:" + e.getLocalizedMessage());
-        }
-    }
-
-    public static <T> T fromYamlValue(String yaml, Class<?> collectionClass, Class<?>... elementClasses) {
-        JavaType javaType = YAML_MAPPER.getTypeFactory().constructParametricType(collectionClass, elementClasses);
-        try {
-            return YAML_MAPPER.readValue(yaml, javaType);
-        } catch (IOException e) {
-            throw new JsonException("convert json error:" + e.getLocalizedMessage());
-        }
-    }
-
-    public static <T> void writeAsYaml(File yamlFile, Class<T> clazz) {
-        try {
-            YAML_MAPPER.writeValue(yamlFile, clazz);
-        } catch (IOException e) {
-            throw new JsonException(e.getLocalizedMessage());
-        }
-    }
-
 
 }
