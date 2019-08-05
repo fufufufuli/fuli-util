e378ad392af5ca667ff6c3e0c304cfba810ffcd1
fuli <137175947672163.com>
2019-08-06 01:37:17 +0800

    feat：      update version

---
+++ src/test/java/com/fuli/util/JsonUtilTest.java
@@ -0,0 +1,14 @@
+package com.fuli.util;
+
+
+import org.junit.Test;
+
+public class JsonUtilTest {
+
+
+    @Test
+    public void test() throws Exception {
+        System.out.println(111);
+    }
+
+}
