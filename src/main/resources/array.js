9724dfaf98c84aa7cecbb14dfb7765af15821aa5
fufufufuli <13717594767@163.com>
2020-04-16 22:25:50 +0800

    google translate api目标结果获得

---
+++ src/main/resources/array.js
@@ -0,0 +1,12 @@
+function array1(str){
+    return str[4];
+}
+function array2(str){
+    return str[5];
+}
+function array3(str){
+    return str[6];
+}
+function array4(str){
+    return str[7];
+}
--- src/test/java/com/fuli/util/UtilTest.java
+++ src/test/java/com/fuli/util/UtilTest.java
@@ -1,14 +1,13 @@
 package com.fuli.util;
 
 
-import com.google.common.collect.Maps;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.io.FileReader;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,21 +16,63 @@ public class UtilTest {
 
     @Test
     public void test() throws Exception {
-        String url = "https://translate.google.cn/translate_a/single";
-        List<String> list = List.of("at", "bd", "ex", "ld", "md", "qcq", "rw", "rm", "sos", "ss", "t");
-        HttpResult httpResult = HttpUtil.get(url, add("blue"), "dt", list);
-        System.out.println(httpResult.getBody());
+
+        String game = transEnglish("床前明月光，疑是地上霜", "en").replaceAll("\n","");
+        String[] split = game.split(",null")[0].split("\",\"");
+        String translate = split[0].substring(4 );
+        String source = split[1].substring(0,split[1].length()-1);
+        System.out.println("================================");
+        System.out.println("source: "+source);
+        System.out.println("translate: "+translate);
+        System.out.println("================================");
+//        String[] array = array(game);
+//        System.out.println("================================");
+//        System.out.println("source: "+array[1]);
+//        System.out.println("translate: "+array[0]);
+//        System.out.println("================================");
+
+
     }
 
-    public Map<String, String> add(String str) {
-        Map<String, String> map = Maps.newTreeMap();
-        map.put("client", "webapp");
-        map.put("sl", "auto");
-        map.put("tl", "zh-CN");
-        map.put("hl", "zh-CN");
-        map.put("tk", token(str));
-        map.put("q", str);
-        return map;
+//    private static String[] array(String value) {
+//
+//        ScriptEngine engine = new ScriptEngineManager().getEngineByName("js");
+//        try (FileReader reader = new FileReader("src/main/resources/array.js")) {
+//            engine.eval(reader);
+//            if (engine instanceof Invocable invoke) {
+//                String array1 = String.valueOf(invoke.invokeFunction("array1", value));
+//                String array2 = String.valueOf(invoke.invokeFunction("array2", value));
+//                String array3 = String.valueOf(invoke.invokeFunction("array3", value));
+//                String array4 = String.valueOf(invoke.invokeFunction("array4", value));
+//
+//
+//                return        array1   .split("=====");
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        throw new RuntimeException();
+//    }
+
+
+
+    public String transEnglish(String str, String target) throws Exception {
+        String url = "https://translate.google.cn/translate_a/single";
+        List<String> list = List.of("at", "bd", "ex", "ld", "md", "qcq", "rw", "rm", "sos", "ss", "t");
+
+        HttpResult httpResult = HttpUtil.get(url, add(str, target), "dt", list);
+        FileUtils.write(FileUtils.getFile("src/main/resources/a"), httpResult.getBody(), "utf8");
+
+        return httpResult.getBody();
+    }
+
+    public Map<String, String> add(String str, String target) {
+        return Map.of("client", "webapp",
+                "sl", "auto",
+                "tl", target,
+                "hl", "zh-CN",
+                "tk", token(str),
+                "q", str);
     }
 
     private static String token(String value) {
